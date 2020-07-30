92b3a0c5c7caa24b99f22f70500d903025ecf3b5
Sunny <yhuse@163.com>
2020-07-30 21:21:47 +0800

    记录更新日志

--- SunnyUI/Controls/UIToolTip.cs
+++ SunnyUI/Controls/UIToolTip.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/******************************************************************************
+ * SunnyUI 开源控件库、工具类库、扩展类库、多页面开发框架。
+ * CopyRight (C) 2012-2020 ShenYongHua(沈永华).
+ * QQ群：56829229 QQ：17612584 EMail：SunnyUI@qq.com
+ *
+ * Blog:   https://www.cnblogs.com/yhuse
+ * Gitee:  https://gitee.com/yhuse/SunnyUI
+ * GitHub: https://github.com/yhuse/SunnyUI
+ *
+ * SunnyUI.dll can be used for free under the GPL-3.0 license.
+ * If you use this code, please keep this note.
+ * 如果您使用此代码，请保留此说明。
+ ******************************************************************************
+ * 文件名称: UIToolTip.cs
+ * 文件说明: 提示
+ * 当前版本: V2.2
+ * 创建日期: 2020-07-21
+ *
+ * 2020-07-21: V2.2.6 增加控件
+ * 2020-07-25: V2.2.6 更新绘制
+******************************************************************************/
+
+using System;
 using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Drawing;
