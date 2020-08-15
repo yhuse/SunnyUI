6f48c21cb04f186d48835b8ea5ceb13bd39ec9cf
Sunny <yhuse@163.com>
2020-08-15 22:49:48 +0800

    * 一些更新

--- SunnyUI/Forms/UILoginForm.Designer.cs
+++ SunnyUI/Forms/UILoginForm.Designer.cs
@@ -37,13 +37,15 @@
             this.lblTitle = new Sunny.UI.UILabel();
             this.lblSubText = new Sunny.UI.UILabel();
             this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiPanel1 = new Sunny.UI.UIPanel();
+            this.uiPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // uiAvatar1
             // 
             this.uiAvatar1.BackColor = System.Drawing.Color.Transparent;
             this.uiAvatar1.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.uiAvatar1.Location = new System.Drawing.Point(498, 136);
+            this.uiAvatar1.Location = new System.Drawing.Point(65, 16);
             this.uiAvatar1.Name = "uiAvatar1";
             this.uiAvatar1.Size = new System.Drawing.Size(60, 60);
             this.uiAvatar1.TabIndex = 4;
@@ -53,7 +55,7 @@
             // 
             this.uiLine1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
             this.uiLine1.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.uiLine1.Location = new System.Drawing.Point(437, 217);
+            this.uiLine1.Location = new System.Drawing.Point(4, 97);
             this.uiLine1.MinimumSize = new System.Drawing.Size(2, 2);
             this.uiLine1.Name = "uiLine1";
             this.uiLine1.Size = new System.Drawing.Size(182, 2);
@@ -65,7 +67,7 @@
             this.edtUser.EnterAsTab = true;
             this.edtUser.FillColor = System.Drawing.Color.White;
             this.edtUser.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.edtUser.Location = new System.Drawing.Point(437, 241);
+            this.edtUser.Location = new System.Drawing.Point(4, 121);
             this.edtUser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.edtUser.Maximum = 2147483647D;
             this.edtUser.Minimum = -2147483648D;
@@ -80,7 +82,7 @@
             this.edtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.edtPassword.FillColor = System.Drawing.Color.White;
             this.edtPassword.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.edtPassword.Location = new System.Drawing.Point(437, 282);
+            this.edtPassword.Location = new System.Drawing.Point(4, 162);
             this.edtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.edtPassword.Maximum = 2147483647D;
             this.edtPassword.Minimum = -2147483648D;
@@ -96,7 +98,7 @@
             // 
             this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLogin.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.btnLogin.Location = new System.Drawing.Point(437, 326);
+            this.btnLogin.Location = new System.Drawing.Point(4, 206);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Padding = new System.Windows.Forms.Padding(28, 0, 0, 0);
             this.btnLogin.Size = new System.Drawing.Size(86, 29);
@@ -112,7 +114,7 @@
             this.btnCancel.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(87)))), ((int)(((byte)(89)))));
             this.btnCancel.FillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(87)))), ((int)(((byte)(89)))));
             this.btnCancel.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.btnCancel.Location = new System.Drawing.Point(533, 326);
+            this.btnCancel.Location = new System.Drawing.Point(100, 206);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Padding = new System.Windows.Forms.Padding(28, 0, 0, 0);
             this.btnCancel.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
@@ -158,12 +160,34 @@
             this.uiLabel1.AutoSize = true;
             this.uiLabel1.BackColor = System.Drawing.Color.Transparent;
             this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.uiLabel1.Location = new System.Drawing.Point(491, 205);
+            this.uiLabel1.Location = new System.Drawing.Point(58, 85);
             this.uiLabel1.Name = "uiLabel1";
             this.uiLabel1.Size = new System.Drawing.Size(74, 21);
             this.uiLabel1.TabIndex = 8;
             this.uiLabel1.Text = "用户登录";
             this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // uiPanel1
+            // 
+            this.uiPanel1.Controls.Add(this.uiAvatar1);
+            this.uiPanel1.Controls.Add(this.uiLabel1);
+            this.uiPanel1.Controls.Add(this.uiLine1);
+            this.uiPanel1.Controls.Add(this.edtUser);
+            this.uiPanel1.Controls.Add(this.edtPassword);
+            this.uiPanel1.Controls.Add(this.btnCancel);
+            this.uiPanel1.Controls.Add(this.btnLogin);
+            this.uiPanel1.FillColor = System.Drawing.Color.White;
+            this.uiPanel1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.uiPanel1.Location = new System.Drawing.Point(433, 126);
+            this.uiPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiPanel1.Name = "uiPanel1";
+            this.uiPanel1.RadiusSides = Sunny.UI.UICornerRadiusSides.None;
+            this.uiPanel1.RectSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.None;
+            this.uiPanel1.Size = new System.Drawing.Size(190, 245);
+            this.uiPanel1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiPanel1.StyleCustomMode = true;
+            this.uiPanel1.TabIndex = 9;
+            this.uiPanel1.Text = null;
             // 
             // UILoginForm
             // 
@@ -171,15 +195,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Sunny.UI.Properties.Resources.Login1;
             this.ClientSize = new System.Drawing.Size(750, 450);
-            this.Controls.Add(this.uiLabel1);
+            this.Controls.Add(this.uiPanel1);
             this.Controls.Add(this.lblSubText);
             this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.edtPassword);
-            this.Controls.Add(this.edtUser);
-            this.Controls.Add(this.uiLine1);
-            this.Controls.Add(this.uiAvatar1);
             this.MaximumSize = new System.Drawing.Size(750, 450);
             this.MinimumSize = new System.Drawing.Size(750, 450);
             this.Name = "UILoginForm";
@@ -188,8 +206,9 @@
             this.ShowTitle = false;
             this.Text = "UILogin";
             this.Enter += new System.EventHandler(this.UILoginForm_Enter);
+            this.uiPanel1.ResumeLayout(false);
+            this.uiPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -199,10 +218,11 @@
         private UILine uiLine1;
         private UITextBox edtUser;
         private UITextBox edtPassword;
-        private UILabel lblSubText;
         protected UILabel lblTitle;
         private UISymbolButton btnLogin;
         private UISymbolButton btnCancel;
         private UILabel uiLabel1;
+        protected UIPanel uiPanel1;
+        protected UILabel lblSubText;
     }
 }
