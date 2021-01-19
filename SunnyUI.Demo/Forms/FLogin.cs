4726defd2a40b5c865a14b902e8e8f99cd0c637a
Sunny <yhuse@163.com>
2021-01-19 22:55:30 +0800

    * 登录窗体Demo增加了一些注释

--- SunnyUI.Demo/Forms/FLogin.cs
+++ SunnyUI.Demo/Forms/FLogin.cs
@@ -9,12 +9,17 @@
 
         private void FLogin_ButtonLoginClick(object sender, System.EventArgs e)
         {
-            this.ShowInfoTip("DSFSDF");
-            // if (UserName == "admin" && Password == "admin")
-            // {
-            //     IsLogin = true;
-            //     Close();
-            // }
+            //UserName就是封装了界面里用户名输入框的值
+            //Password就是封装了界面里密码输入框的值
+            if (UserName == "admin" && Password == "admin")
+            {
+                IsLogin = true;
+                Close();
+            }
+            else
+            {
+                ShowErrorTip("用户名或者密码错误。");
+            }
         }
     }
 }
