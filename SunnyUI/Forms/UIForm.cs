d95d03928017557bfff0ab9c67905ae1f961c570
Sunny <yhuse@163.com>
2021-02-20 15:45:47 +0800

    * 修改版权信息

--- SunnyUI/Forms/UIForm.cs
+++ SunnyUI/Forms/UIForm.cs
@@ -1,7 +1,7 @@
 /******************************************************************************
  * SunnyUI 开源控件库、工具类库、扩展类库、多页面开发框架。
- * CopyRight (C) 2012-2020 ShenYongHua(沈永华).
- * QQ群：56829229 QQ：17612584 EMail：SunnyUI@qq.com
+ * CopyRight (C) 2012-2021 ShenYongHua(沈永华).
+ * QQ群：56829229 QQ：17612584 EMail：SunnyUI@QQ.Com
  *
  * Blog:   https://www.cnblogs.com/yhuse
  * Gitee:  https://gitee.com/yhuse/SunnyUI
