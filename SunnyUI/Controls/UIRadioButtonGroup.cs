118feaaddb7956bbc0f3ba08e73c66e6c1d4ab2b
Sunny <yhuse@163.com>
2021-04-20 17:03:35 +0800

    * 更新WIKI时，对一些文件的描述也做了更新。

--- SunnyUI/Controls/UIRadioButtonGroup.cs
+++ SunnyUI/Controls/UIRadioButtonGroup.cs
@@ -56,6 +56,14 @@ namespace Sunny.UI
             buttons.Clear();
         }
 
+        public void Clear()
+        {
+            Items.Clear();
+            ClearButtons();
+            SelectedIndex = -1;
+            Invalidate();
+        }
+
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
         [Localizable(true)]
         [Editor("System.Windows.Forms.Design.ListControlStringCollectionEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
