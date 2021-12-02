49936e04dd1e7604a8d835165b1065b53fea7d45
Sunny <yhuse@163.com>
2021-12-02 09:13:06 +0800

    * IFrame：创建文档

--- SunnyUI/Frames/IFrame.cs
+++ SunnyUI/Frames/IFrame.cs
@@ -1,8 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/******************************************************************************
+ * SunnyUI 开源控件库、工具类库、扩展类库、多页面开发框架。
+ * CopyRight (C) 2012-2021 ShenYongHua(沈永华).
+ * QQ群：56829229 QQ：17612584 EMail：SunnyUI@QQ.Com
+ *
+ * Blog:   https://www.cnblogs.com/yhuse
+ * Gitee:  https://gitee.com/yhuse/SunnyUI
+ * GitHub: https://github.com/yhuse/SunnyUI
+ *
+ * SunnyUI.dll can be used for free under the GPL-3.0 license.
+ * If you use this code, please keep this note.
+ * 如果您使用此代码，请保留此说明。
+ ******************************************************************************
+ * 文件名称: IFrame.cs
+ * 文件说明: 页面框架接口
+ * 当前版本: V3.0
+ * 创建日期: 2020-12-01
+ *
+ * 2021-12-01: V3.0.9 创建文档
+******************************************************************************/
+
+using System;
 
 namespace Sunny.UI
 {
