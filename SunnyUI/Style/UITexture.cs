b346a8e15642065376ac204c307f93e49cf1d1cc
Sunny <yhuse@163.com>
2021-12-10 10:58:24 +0800

    + UITexture：增加主题背景纹理类

---
+++ SunnyUI/Style/UITexture.cs
@@ -0,0 +1,35 @@
+﻿/******************************************************************************
+ * SunnyUI 开源控件库、工具类库、扩展类库、多页面开发框架。
+ * CopyRight (C) 2012-2021 ShenYongHua(沈永华).
+ * QQ群：56829229 QQ：17612584 EMail：SunnyUI@QQ.Com
+ *
+ * Blog:   https://www.cnblogs.com/yhuse
+ * Gitee:  https://gitee.com/yhuse/SunnyUI
+ * GitHub: https://github.com/yhuse/SunnyUI
+ *
+ * SunnyUI.dll can be used for free under the GPL-3.0 license.
+ * If you use this code, please keep this note.
+ * 如果您使用此代码，请保留此说明。
+ ******************************************************************************
+ * 文件名称: UITexture.cs
+ * 文件说明: 主题背景纹理类
+ * 当前版本: V3.0
+ * 创建日期: 2020-12-10
+ *
+ * 2020-01-01: V3.0.9 增加文件说明
+******************************************************************************/
+
+using System.Drawing;
+
+namespace Sunny.UI
+{
+    public static class UITexture
+    {
+        public static TextureBrush CreateTextureBrush(Image img)
+        {
+            TextureBrush tb = new TextureBrush(img);
+            tb.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
+            return tb;
+        }
+    }
+}
