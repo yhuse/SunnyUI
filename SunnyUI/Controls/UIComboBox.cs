66f752fccc05afc9ca8607fcaac0186ad1f4a364
Sunny <yhuse@163.com>
2022-07-21 21:51:53 +0800

    * UIComboBox: 屏蔽FormatString的编辑器

--- SunnyUI/Controls/UIComboBox.cs
+++ SunnyUI/Controls/UIComboBox.cs
@@ -689,7 +689,6 @@ namespace Sunny.UI
 
         [Description("获取或设置指示显示值的方式的格式说明符字符。"), Category("SunnyUI")]
         [DefaultValue("")]
-        [Editor("System.Windows.Forms.Design.FormatStringEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
         [MergableProperty(false)]
         public string FormatString
         {
