3c2ba689ebdd79c91c0f1bd099d7e6898b8c8fa9
Sunny <yhuse@163.com>
2024-11-15 10:30:17 +0800

    更新

--- SunnyUI/Charts/UILineChart.cs
+++ SunnyUI/Charts/UILineChart.cs
@@ -60,8 +60,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Sunny.UI
 {
