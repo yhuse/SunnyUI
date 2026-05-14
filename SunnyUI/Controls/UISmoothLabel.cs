500092363493e7cf5d093911653b061908afca98
yhuse <yhuse@163.com>
2026-05-14 23:23:21 +0800

    * GraphicsEx: 缓存了常用的Pen和Brush对象，避免重复创建

--- SunnyUI/Controls/UISmoothLabel.cs
+++ SunnyUI/Controls/UISmoothLabel.cs
@@ -1,24 +1,24 @@
 ﻿/******************************************************************************
- * SunnyUI 开源控件库、工具类库、扩展类库、多页面开发框架。
- * CopyRight (C) 2012-2026 ShenYongHua(沈永华).
- * QQ群：56829229 QQ：17612584 EMail：SunnyUI@QQ.Com
- *
- * Blog:   https://www.cnblogs.com/yhuse
- * Gitee:  https://gitee.com/yhuse/SunnyUI
- * GitHub: https://github.com/yhuse/SunnyUI
- *
- * SunnyUI.dll can be used for free under the GPL-3.0 license.
- * If you use this code, please keep this note.
- * 如果您使用此代码，请保留此说明。
- ******************************************************************************
- * 文件名称: UISmoothLabel.cs
- * 文件说明: 平滑文字带边框的标签
- * 当前版本: V3.1
- * 创建日期: 2022-01-22
- *
- * 2022-01-22: V3.1.0 增加文件说明
- * 2022-03-19: V3.1.1 重构主题配色
- * 2023-11-16: V3.5.2 重构主题
+* SunnyUI 开源控件库、工具类库、扩展类库、多页面开发框架。
+* CopyRight (C) 2012-2026 ShenYongHua(沈永华).
+* QQ群：56829229 QQ：17612584 EMail：SunnyUI@QQ.Com
+*
+* Blog:   https://www.cnblogs.com/yhuse
+* Gitee:  https://gitee.com/yhuse/SunnyUI
+* GitHub: https://github.com/yhuse/SunnyUI
+*
+* SunnyUI.dll can be used for free under the GPL-3.0 license.
+* If you use this code, please keep this note.
+* 如果您使用此代码，请保留此说明。
+******************************************************************************
+* 文件名称: UISmoothLabel.cs
+* 文件说明: 平滑文字带边框的标签
+* 当前版本: V3.1
+* 创建日期: 2022-01-22
+*
+* 2022-01-22: V3.1.0 增加文件说明
+* 2022-03-19: V3.1.1 重构主题配色
+* 2023-11-16: V3.5.2 重构主题
 ******************************************************************************/
 
 
@@ -52,6 +52,7 @@ namespace Sunny.UI
 
             drawPath = new GraphicsPath();
             drawPen = new Pen(rectColor, rectSize);
+            forecolorBrush = new SolidBrush(ForeColor);
             Size = new Size(300, 60);
         }
 
